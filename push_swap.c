0859928d9edf82ad160fe67dc708a8061d6b41e1
Davit Grigoryan <dgrigor2@c2r10s4.42yerevan.am>
2025-03-18 16:04:50 +0400

    pushing ot grexa podalshe

---
+++ push_swap.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgrigor2 <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 16:05:49 by dgrigor2          #+#    #+#             */
+/*   Updated: 2025/03/17 16:05:51 by dgrigor2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	*parser(int argc, char **argv)
+{
+	int	i;
+	int	*res;
+
+	i = 0;
+	res = (int *)malloc(sizeof(int) * (argc - 1));
+	if (!res)
+		return (NULL);
+	while (i < argc - 1)
+	{
+		res[i] = ft_atoi(argv[i]);
+		if (res[i] == 0 && argv[i][0] != '0')
+		{
+			free(res);
+			return (NULL);
+		}
+		i++;
+	}
+	return (res);
+}
+
+int	main(int argc, char **argv)
+{
+	int *arr = parser(argc, argv);
+	if (!arr)
+	{
+		write (1, "Gjvel es?\n", 10);
+		return (0);
+	}
+	while (argc > 1)
+	{
+		printf("%d\n", arr[argc]);
+		argc--;
+	}
+	return (0);
+
+}
+
+// void	ra(int argc, char **argv)
+// {
+// 	char	*swap = argv[1];
+// 	int		i;
+	
+// 	i = 2;
+// 	while ()
+
+// }
+
+// void	rra
+
+// void	push_swap(int argc, char **argv)
+// {
+//     char	**stack_a;
+//     char	**stack_b;
+//     int		a_len;
+//     int		b_len;
+
+// 	stack_a = (char **)malloc((argc - 1) + sizeof(char *));
+// 	if (!stack_a)
+// 		return ;
+// 	stack_b = (char **)malloc((argc - 1) * sizeof(char *));
+// 	if (!stack_b)
+// 	{
+// 		free(stack_a);
+// 		return ;
+// 	}
+// 	a_len = argc - 1;
+// 	b_len = 0;
+// 	while (a_len > 0)
+// 	{
+
+// 	}
+// }
