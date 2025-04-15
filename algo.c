ae9e7c8e452d78a1d128e736aad2b988e4b72ae5
Davit Grigoryan <dgrigor2@c2r11s5.42yerevan.am>
2025-04-15 18:33:15 +0400

    smth

--- algo.c
+++ algo.c
@@ -1,15 +1,32 @@
 #include "push_swap.h"
 
+int	puchur(t_list *lst)
+{
+	int	min;
+	
+	min = lst->content;
+	while (lst)
+	{
+		if (lst->content < min)
+			min = lst->content;
+		lst = lst->min;
+	}
+	return (min);
+}
+
 void	frik(t_list **a, t_list **b)
 {
 	int	i;
 	int	size;
+	int	margin;
 
+	margin = puchur(*a) - 1;
 	i = 0;
 	size = ft_lstsize(*a);
+	ft_printf("\n\nsize: %d\n\n", size);
 	while (i < size)
 	{
-		if ((*a)->content > 2 * size / 3)
+		if ((*a)->content > 2 * size / 3 + margin)
 		{
 			ra(a);
 		}
@@ -32,5 +49,6 @@ void	algo(t_list **a, t_list **b)
 
 	size = ft_lstsize(*a);
 	//while (ft_lstsize(*a) > 2)
+	frik(a, b);
 	frik(a, b);
 }
